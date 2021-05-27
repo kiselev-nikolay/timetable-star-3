2d5dd08f96f753a5e4d6135abf0922a4f28cc1ec
Nikolay Kiselev <55307887+kiselev-nikolay@users.noreply.github.com>
2021-05-27 13:50:41 +0300

    Init project

--- app.go
+++ app.go
@@ -1,4 +1,16 @@
 package main
 
+import "syscall/js"
+
 func main() {
+	js.Global().Set("render", Render())
+	select {}
+}
+
+func Render() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		data := args[0].String()
+		data = "!!" + data + "!!"
+		return data
+	})
 }
